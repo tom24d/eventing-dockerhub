826d50a60847ad859eb35fed654013fbb145f75a
Tomoya Nishide <tom.24d.en@gmail.com>
2020-06-06 22:39:04 +0900

    add unit test for v1alpha1

--- pkg/apis/sources/v1alpha1/dockerhubsource_defaults_test.go
+++ pkg/apis/sources/v1alpha1/dockerhubsource_defaults_test.go
@@ -1,9 +1,13 @@
 package v1alpha1
 
 import (
+	"context"
 	"testing"
 )
 
 func TestDockerhubSourceDefaults(t *testing.T) {
-	// TODO None yet
+	// None yet
+	d := &DockerHubSource{}
+	d.SetDefaults(context.TODO())
+	d.Spec.SetDefaults(context.TODO())
 }
--- pkg/apis/sources/v1alpha1/dockerhubsource_validation.go
+++ pkg/apis/sources/v1alpha1/dockerhubsource_validation.go
@@ -6,13 +6,12 @@ import (
 	"knative.dev/pkg/apis"
 )
 
-// TODO
-// Validate validates SampleSource.
+// Validate validates DockerHubSource.
 func (s *DockerHubSource) Validate(ctx context.Context) *apis.FieldError {
 	var errs *apis.FieldError
 
-	//example: validation for "spec" field.
-	//errs = errs.Also(s.Spec.Validate(ctx).ViaField("spec"))
+	//validation for "spec" field.
+	errs = errs.Also(s.Spec.Validate(ctx).ViaField("spec"))
 
 	//errs is nil if everything is fine.
 	return errs
@@ -20,8 +19,13 @@ func (s *DockerHubSource) Validate(ctx context.Context) *apis.FieldError {
 
 // Validate validates SampleSourceSpec.
 func (sspec *DockerHubSourceSpec) Validate(ctx context.Context) *apis.FieldError {
-	//Add code for validation webhook for SampleSourceSpec.
+	//Add code for validation webhook for DockerHubSourceSpec.
 	var errs *apis.FieldError
+
+	//validation for sink field.
+	if fe := sspec.Sink.Validate(ctx); fe != nil {
+		errs = errs.Also(fe.ViaField("sink"))
+	}
 
 	return errs
 }
