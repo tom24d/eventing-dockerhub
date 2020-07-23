acf511cb81699f5ec40b9bc4e24ee11d078ce44e
Tomoya Nishide <tom.24d.en@gmail.com>
2020-07-23 16:23:44 +0900

    remove dead code

--- test/resources/marshaler.go
+++ test/resources/marshaler.go
@@ -2,17 +2,9 @@ package resources
 
 import (
 	"encoding/json"
-
-	dockerhub "gopkg.in/go-playground/webhooks.v5/docker"
 )
 
 func MarshalPayload(payload interface{}) string {
 	byteData, _ := json.Marshal(payload)
 	return string(byteData)
-}
-
-func UnmarshalPayload(str string) dockerhub.BuildPayload {
-	var p dockerhub.BuildPayload
-	_ = json.Unmarshal([]byte(str), &p)
-	return p
 }
