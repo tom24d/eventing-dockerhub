3db8addc2d94fe2bc7ab2902fdee938d173b196d
Tomoya Nishide <tom.24d.en@gmail.com>
2020-08-16 00:44:35 +0900

    increase timeout to 6m

--- test/kind/run-tests.sh
+++ test/kind/run-tests.sh
@@ -25,6 +25,6 @@ source "$(dirname $0)/../e2e-common.sh"
 
 initialize $@ --skip-istio-addon --run-tests --skip-knative-setup --skip-teardowns
 
-go_test_e2e -timeout=5m ./test/e2e -tag e2e || fail_test
+go_test_e2e -timeout=6m ./test/e2e -tag e2e || fail_test
 
 success
