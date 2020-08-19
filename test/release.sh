1490e4eab0a918fe460a916fb8f7f99c479a50c8
Tomoya Nishide <tom.24d.en@gmail.com>
2020-08-19 13:00:02 +0900

    fix release sh

--- test/release.sh
+++ test/release.sh
@@ -1,9 +1,11 @@
-source vendor/knative.dev/test-infra/scripts/release.sh
+#!/usr/bin/env bash
+
+#source vendor/knative.dev/test-infra/scripts/release.sh
 
 function build_release() {
   # config/ contains the manifests
   ko resolve ${KO_FLAGS} -f config/ > release.yaml
   ARTIFACTS_TO_PUBLISH="release.yaml"
 }
-
-main "$@"
+# TODO
+#main "$@"
