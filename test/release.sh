8db8c3b0d3d89de36615a9e6118db7d886ed31e7
Tomoya Nishide <tom.24d.en@gmail.com>
2020-08-19 20:04:41 +0900

    replace release tag (#119)

--- test/release.sh
+++ test/release.sh
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
 
+# (tom24d) donotpush to org repo
+
 if [[ ! -v TAG ]]; then
   TAG="nightly"
   readonly TAG
 fi
 
 function build_release() {
+  # Update release labels if this is a tagged release
+  if [[ -n "${TAG}" ]]; then
+    echo "Tagged release, updating release labels to contrib.eventing.knative.dev/release: \"${TAG}\""
+    LABEL_YAML_CMD=(sed -e "s|contrib.eventing.knative.dev/release: devel|contrib.eventing.knative.dev/release: \"${TAG}\"|")
+  else
+    echo "Untagged release, will NOT update release labels"
+    LABEL_YAML_CMD=(cat)
+  fi
+
   # config/ contains the manifests
-  ko resolve --tags "$1" -f config/ > release.yaml
+  ko resolve --tags "$1" -f config/ | "${LABEL_YAML_CMD[@]}" > release.yaml
   ARTIFACTS_TO_PUBLISH="release.yaml"
 }
 
